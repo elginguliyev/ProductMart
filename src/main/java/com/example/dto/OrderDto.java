1ef9762b3244c22f7fcb72e53a60db859b1f3226
elgin <elgin,guliyev@gmail.com>
2024-08-19 12:09:08 -0700

    Master Branch

--- src/main/java/com/example/dto/OrderDto.java
+++ src/main/java/com/example/dto/OrderDto.java
@@ -1,42 +1,21 @@
 package com.example.dto;
 
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
 
     private Long userId;
+
     private List<OrderItemDto> orderItems;
 
-    public OrderDto() {
-    }
+    private Double totalPrice;
 
 
-    public OrderDto(Long userId, List<OrderItemDto> orderItems) {
-        this.userId = userId;
-        this.orderItems = orderItems;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public List<OrderItemDto> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setOrderItems(List<OrderItemDto> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderDto{" +
-                "userId=" + userId +
-                ", orderItems=" + orderItems +
-                '}';
-    }
 }
--- src/main/java/com/example/dto/OrderItemDto.java
+++ src/main/java/com/example/dto/OrderItemDto.java
@@ -1,50 +1,18 @@
 package com.example.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDto {
 
     private Long productId;
+
     private int quantity;
+
     private double price;
 
-    public OrderItemDto() {
-    }
-
-    public OrderItemDto(Long productId, int quantity, double price) {
-        this.productId = productId;
-        this.quantity = quantity;
-        this.price = price;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderItemDto{" +
-                "productId=" + productId +
-                ", quantity=" + quantity +
-                ", price=" + price +
-                '}';
-    }
 }
