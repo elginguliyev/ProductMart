254b20ad74f9384fd60cc21d04c759952d964d3d
elgin <elgin.guliyev@gmail.com>
2024-08-30 08:28:19 -0700

    Controllers

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,15 +1,25 @@
 package com.example.controller;
 
+import com.example.entites.Product;
 import com.example.services.ProductsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api/v1/product")
+@RequestMapping(path = "/api/v1/")
 public class ProductController {
 
     @Autowired
     ProductsServices productsServices;
+
+    @GetMapping(path = "product")
+    public List<Product> getAllproducts() {
+        return productsServices.getAllProducts();
+    }
+
 
 }
