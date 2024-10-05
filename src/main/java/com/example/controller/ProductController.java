9ce34254e57b350ac7b13dca5fbbf8df7ed51190
elgin <elgin.guliyev@gmail.com>
2024-10-05 05:30:17 -0700

    Controllers

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -3,23 +3,35 @@ package com.example.controller;
 import com.example.entites.Product;
 import com.example.services.ProductsServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/")
+@RequestMapping(path = "/api/v1")
 public class ProductController {
 
     @Autowired
     ProductsServices productsServices;
 
-    @GetMapping(path = "product")
-    public List<Product> getAllproducts() {
-        return productsServices.getAllProducts();
+    @GetMapping(path = "/all/product")
+    public ResponseEntity<List<Product>> getAllproducts() {
+        List<Product> productList = productsServices.getAllProducts();
+        return ResponseEntity.ok(productList);
     }
 
+
+    @GetMapping(path = "/{id}/product")
+    public ResponseEntity<Product> getByIdProduct(@PathVariable Long id) {
+        Product product = productsServices.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        productsServices.deleteProduct(id);
+        return ResponseEntity.ok("User with ID " + id + " deleted successfully");
+    }
 
 }
--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.controller;
 
+import com.example.entites.User;
 import com.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/user")
@@ -13,4 +16,21 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @GetMapping("/all/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}/user")
+    public ResponseEntity<User> getByIdUser(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteByUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User with ID " + id + " deleted successfully");
+    }
 }
