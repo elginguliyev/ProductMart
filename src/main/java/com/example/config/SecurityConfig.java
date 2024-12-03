9a3389de49faabb63b7107a5cf13ccd8a970831d
Elgin <81240513+elginguliyev@users.noreply.github.com>
2024-12-03 15:56:12 +0400

    WebConfig

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/admin/**", "/api/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/uploads/**").permitAll()
 
                                 .anyRequest().authenticated()
                 )
